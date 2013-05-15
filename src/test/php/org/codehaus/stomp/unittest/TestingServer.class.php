e4a3af25b04b42904eb39c9a48addd7f2ee7afd4
Timm Friebe <thekid@thekid.de>
2013-05-15 10:44:28 +0200

    - Migrate to new coding standards

--- src/test/php/org/codehaus/stomp/unittest/TestingServer.class.php
+++ src/test/php/org/codehaus/stomp/unittest/TestingServer.class.php
@@ -1,152 +1,148 @@
-<?php
-/* This class is part of the XP framework
+<?php namespace org\codehaus\stomp\unittest;
+
+use util\cmd\Console;
+use util\log\LogCategory;
+use util\log\FileAppender;
+use peer\server\Server;
+
+/**
+ * STOMP Server used by IntegrationTest. 
  *
- * $Id$
+ * @see   xp://org.codehaus.stomp.unittest.StompIntegrationTest
  */
+class TestingServer extends \lang\Object {
 
-  $package= 'org.codehaus.stomp.unittest';
-
-  uses(
-    'util.cmd.Console',
-    'util.log.Logger',
-    'util.log.FileAppender',
-    'peer.server.Server',
-    'peer.server.ServerProtocol',
-    'org.codehaus.stomp.frame.Frame'
-  );
-  
   /**
-   * STOMP Server used by IntegrationTest. 
+   * Start server
    *
-   * @see   xp://org.codehaus.stomp.unittest.StompIntegrationTest
+   * @param   string[] args
    */
-  class org·codehaus·stomp·unittest·TestingServer extends Object {
+  public static function main(array $args) {
 
-    /**
-     * Start server
-     *
-     * @param   string[] args
-     */
-    public static function main(array $args) {
+    // Define server frame
+    \lang\XPClass::forName('peer.stomp.frame.Frame');
+    \lang\ClassLoader::defineClass('peer.stomp.frame.ConnectFrame', 'peer.stomp.frame.Frame', array(), '{
+      public function command() { return "CONNECT"; }
+    }');
 
-      // Server frames
-      ClassLoader::defineClass('org.codehaus.stomp.frame.ConnectFrame', 'org.codehaus.stomp.frame.Frame', array(), '{
-        public function command() { return "CONNECT"; }
-      }');
+    // Define protocol
+    $protocol= newinstance('peer.server.ServerProtocol', array(), '{
+      protected $frames= null;
+      protected $handlers= array();
+      protected $cat= null;
+      public $messages= array();
 
-      $protocol= newinstance('peer.server.ServerProtocol', array(), '{
-        protected $frames= NULL;
-        protected $handlers= array();
-        protected $cat= NULL;
-        public $messages= array();
-
-        public function initialize() { 
-          $this->frames= Package::forName("org.codehaus.stomp.frame");
-          $this->handlers= array(
-            "CONNECT" => function($frame, $protocol) {
-              if ("testtest" === $frame->getHeader("login").$frame->getHeader("passcode")) {
-                return $protocol->frame("CONNECTED");
-              } else {
-                return $protocol->frame("ERROR");
-              }
-            },
-
-            "SEND" => function($frame, $protocol) {
-              if ("/queue/test" === $frame->getHeader("destination")) {
-                $protocol->messages[]= $frame->getBody();
-                return NULL;
-              } else {
-                return $protocol->frame("ERROR");
-              }
-            },
-
-            "SUBSCRIBE" => function($frame, $protocol) {
-              if ("/queue/test" === $frame->getHeader("destination")) {
-                $i= 1;
-                $messages= array();
-                while ($dequeued= array_shift($protocol->messages)) {
-                  $message= $protocol->frame("message");
-                  $message->addHeader("message-id", $i++);
-                  $message->setBody($dequeued);
-                  $messages[]= $message;
-                }
-                return $messages;
-              } else {
-                return $protocol->frame("ERROR");
-              }
+      public function initialize() { 
+        $this->frames= Package::forName("peer.stomp.frame");
+        $this->handlers= array(
+          "CONNECT" => function($frame, $protocol) {
+            if ("testtest" === $frame->getHeader("login").$frame->getHeader("passcode")) {
+              return $protocol->frame("CONNECTED");
+            } else {
+              return $protocol->frame("ERROR");
             }
-          );
-        }
+          },
 
-        public function handleConnect($socket) {
-          $this->cat && $this->cat->debug("Connect", $socket);
-        }
+          "SEND" => function($frame, $protocol) {
+            if ("/queue/test" === $frame->getHeader("destination")) {
+              $protocol->messages[]= $frame->getBody();
+              return null;
+            } else {
+              return $protocol->frame("ERROR");
+            }
+          },
 
-        public function handleDisconnect($socket) {
-          $this->cat && $this->cat->debug("Disconnect", $socket);
-        }
-
-        public function frame($name) {
-          return $this->frames->loadClass(ucfirst(strtolower($name))."Frame")->newInstance();
-        }
-
-        public function handleData($socket) {
-          $line= $socket->readLine();
-
-          if ("SHUTDOWN" === $line) {
-            $this->server->terminate= TRUE;
-            return;
-          } else if ("" == $line) {
-            // Nothing
-            return;
+          "SUBSCRIBE" => function($frame, $protocol) {
+            if ("/queue/test" === $frame->getHeader("destination")) {
+              $i= 1;
+              $messages= array();
+              while ($dequeued= array_shift($protocol->messages)) {
+                $message= $protocol->frame("message");
+                $message->addHeader("message-id", $i++);
+                $message->setBody($dequeued);
+                $messages[]= $message;
+              }
+              return $messages;
+            } else {
+              return $protocol->frame("ERROR");
+            }
           }
+        );
+      }
 
-          $request= $this->frame($line);
-          $request->fromWire(new StringReader($socket->getInputStream()));
-          $this->cat && $this->cat->info("<<<", $request);
+      public function handleConnect($socket) {
+        $this->cat && $this->cat->debug("Connect", $socket);
+      }
 
-          if ($handler= $this->handlers[$request->command()]) {
+      public function handleDisconnect($socket) {
+        $this->cat && $this->cat->debug("Disconnect", $socket);
+      }
+
+      public function frame($name) {
+        return $this->frames->loadClass(ucfirst(strtolower($name))."Frame")->newInstance();
+      }
+
+      public function handleData($socket) {
+        $line= $socket->readLine();
+
+        if ("SHUTDOWN" === $line) {
+          $this->server->terminate= TRUE;
+          return;
+        } else if ("" == $line) {
+          // Nothing
+          return;
+        }
+
+        $request= $this->frame($line);
+        $request->fromWire(new StringReader($socket->getInputStream()));
+        $this->cat && $this->cat->info("<<<", $request);
+
+        if ($handler= $this->handlers[$request->command()]) {
+          try {
             $response= $handler($request, $this);
-          } else {
+          } catch (Throwable $e) {
+            $this->cat && $this->cat->warn("*** ", $e);
             $response= $this->frame("ERROR");
           }
+        } else {
+          $response= $this->frame("ERROR");
+        }
 
-          $this->cat && $this->cat->info(">>>", $response);
-          if (NULL === $response) {
-            return NULL;
-          } else if (is_array($response)) {
-            $out= new StringWriter($socket->getOutputStream());
-            foreach ($reponse as $framne) {
-              $frame->write($out);
-            }
-          } else {
-            $response->write(new StringWriter($socket->getOutputStream()));
+        $this->cat && $this->cat->info(">>>", $response);
+        if (null === $response) {
+          return null;
+        } else if (is_array($response)) {
+          $out= new StringWriter($socket->getOutputStream());
+          foreach ($reponse as $frame) {
+            $frame->write($out);
           }
+        } else {
+          $response->write(new StringWriter($socket->getOutputStream()));
         }
-
-        public function handleError($socket, $e) {
-        }
-
-        public function setTrace($cat) {
-          $this->cat= $cat;
-        }
-      }');
-
-      // Trace file
-      if (isset($args[0])) {
-        $protocol->setTrace(create(new LogCategory())->withAppender(new FileAppender($args[0])));
       }
 
-      $s= new Server('127.0.0.1', 0);
-      try {
-        $s->setProtocol($protocol);
-        $s->init();
-        Console::writeLinef('+ Service %s:%d', $s->socket->host, $s->socket->port);
-        $s->service();
-        Console::writeLine('+ Done');
-      } catch (Throwable $e) {
-        Console::writeLine('- ', $e->getMessage());
+      public function handleError($socket, $e) {
       }
+
+      public function setTrace($cat) {
+        $this->cat= $cat;
+      }
+    }');
+
+    // Trace file
+    if (isset($args[0])) {
+      $protocol->setTrace(create(new LogCategory())->withAppender(new FileAppender($args[0])));
+    }
+
+    $s= new Server('127.0.0.1', 0);
+    try {
+      $s->setProtocol($protocol);
+      $s->init();
+      Console::writeLinef('+ Service %s:%d', $s->socket->host, $s->socket->port);
+      $s->service();
+      Console::writeLine('+ Done');
+    } catch (Throwable $e) {
+      Console::writeLine('- ', $e->getMessage());
     }
   }
-?>
+}
